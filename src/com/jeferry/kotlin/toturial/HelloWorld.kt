7cd60553bc2958522208d2b1d2779f0507724ac4
chenmingzhi <chenmingzhi@ccclubs.com>
2019-10-12 20:18:06 +0800

    hello kotlin

---
+++ src/com/jeferry/kotlin/toturial/HelloWorld.kt
@@ -0,0 +1,13 @@
+package com.jeferry.kotlin.toturial
+
+var i = 0
+
+fun main(args: Array<String>) {
+    println("hello kotlin")
+    println(++i)
+    print(9)
+}
+
+fun print(a: Int) {
+    println("a : $a")
+}
